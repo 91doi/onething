ab6323996a619071cdb2dc174daacb9f041f9e1a
91doi <97842192+91doi@users.noreply.github.com>
2021-10-22 16:02:26 +0000

    Remove extra quote

--- 0.15/alpine/docker-entrypoint.sh
+++ 0.15/alpine/docker-entrypoint.sh
@@ -4,3 +4,8 @@ set -e
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for bgoldd"
+
+  set -- bgoldd "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bgoldd" ]; then
