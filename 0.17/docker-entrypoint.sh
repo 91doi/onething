380427aa07e89484802e721df0f50da23e02c7a8
91doi <97842192+91doi@users.noreply.github.com>
2022-06-04 20:59:50 +0000

    Use WARN_ON_ONCE for missing X86_FEATURE_NRIPS

--- 0.17/docker-entrypoint.sh
+++ 0.17/docker-entrypoint.sh
@@ -7,4 +7,4 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
   set -- bgoldd "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bgoldd
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bgoldd" 
